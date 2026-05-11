946489a110b7ab7ada62b145736fbbe4bf40de7c
mibokiss-coder <mibokiss@gmail.com>
2026-05-11 20:47:19 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+// 每次更新 index.html 時，更新這個版本號讓 SW 強制重新快取
+const CACHE = 'cat-health-v3';
+const FILES = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network first，成功就更新快取；離線才用快取
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
